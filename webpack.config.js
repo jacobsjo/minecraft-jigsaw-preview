6a78cdfe868884956eb208b5c3668cdf145954f9
Jochen Jacobs <jochen.jacobs@campus.tu-berlin.de>
2022-11-20 03:49:53 +0100

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,71 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-var-requires */
-var CopyWebpackPlugin = require('copy-webpack-plugin')
-const webpack = require('webpack')
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
+const webpack = require('webpack');
 
-module.exports = (env, argv) => ({
-  entry: './src/App.ts',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'js/bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader' }
-    ]
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public', to: '' }
-      ]
-    }) 
-   ]
-})
+
+module.exports = {
+    entry: {
+        bundle: './src/App.ts',
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.glsl$/,
+            loader: 'webpack-glsl-loader'
+        } 
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        fallback: {
+        }
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },    
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public', to: '' }
+            ]
+        }),
+        new LicenseWebpackPlugin({
+            licenseTextOverrides: {
+                uniqid: 
+`MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`
+            },
+            perChunkOutput: false,
+            outputFilename: 'licenses.txt'
+        })
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    }
+};
